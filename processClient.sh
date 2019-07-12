ecf7722edfbca630bef76383cf514fbd52c71349
metallicOxide <47731814+metallicOxide@users.noreply.github.com>
2019-07-12 21:25:54 +1000

    added comments to make the code more readable

--- processClient.sh
+++ processClient.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sed -r 's/^.*",,"//g;s/^.*",[0-9],"//g;s/""/"/g;s/"$//g' 'clinical notes_2019_07_08_14-44.csv' | tail -n +2 > clientNotes.json
+# CSV given in format of
+
+# "client name",,,"client number",,"JSON OBJECT STRING"
+# remove everything till the ",," deliminator
+# proceeds to remove trailing " at the end
+
+sed -r 's/^.*",,"//g;s/^.*",[0-9],"//g;s/""/"/g;s/"$//g' 'clinical notes_2019_07_08_14-44.csv' 
+  | tail -n +2 > clientNotes.json
 
