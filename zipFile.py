5aefbe5f6db08aa32051bd13d94fe16244edede7
metallicOxide <47731814+metallicOxide@users.noreply.github.com>
2019-10-27 23:38:19 +1100

    extended script to rename files that are than skyvia limit

--- zipFile.py
+++ zipFile.py
@@ -1,12 +1,33 @@
-import os
+import os, re
 from shutil import copy
+import json
+import pandas as pd
 
 MAX_FOLDER_SIZE = 400*1024*1024
 MAX_FILE_SIZE = 4*1024*1024
-DOCUMENT_ROOT_PATH = "C:\\Users\\usr\\Downloads\\ClientDocs"
-ZIP_SAVE_PATH = "C:\\Users\\usr\\Desktop\\ZIP"
-OVERSIZE_SAVE_PATH = "C:\\Users\\usr\\Desktop\\Oversize"
+
+# ############## change below for non testing ################################
+DOCUMENT_ROOT_PATH = "C:\\Users\\jerry lu\\Downloads\\ClientDocs"
+# DOCUMENT_ROOT_PATH = "C:\\Users\\jerry lu\\Downloads\\test"
+##############################################################################
+
+ZIP_SAVE_PATH = "C:\\Users\\jerry lu\\Desktop\\ZIP"
+OVERSIZE_SAVE_PATH = "C:\\Users\\jerry lu\\Desktop\\Oversize"
 FILE_INDEX = "fileindex.txt"
+JSON_FILE = 'clients_exported.json'
+MAX_STRING_LENGTH = 70
+
+def parseJsonToIntArr():
+    file = open('clients_exported.json')
+    json_arr = json.load(file)
+    num_arr = list(map(int, json_arr))
+    num_arr.sort()
+    return num_arr
+
+def read_client_ids():
+    df = pd.read_excel ('test_client_ids.xlsx') # csv qill not work if we are trying to read by column name. Use .xlsx for now
+	# Returns an array of client IDs
+    return df['ClientID']
 
 # counts FileSize in src path given, ignores 
 def countFileSize(src):
@@ -21,8 +42,31 @@ def countFileSize(src):
                 totalSize += os.path.getsize(docPath)
     return totalSize
 
+def writeToTooLong(filePath):
+    f = open("fileLength.txt", "a+")
+    f.write(filePath+"\n")
+    f.close()
+
 # copies from src to dest and appends fileID to name of src file
 def copyFileToDest(src, dest, fileID, docName):
+
+    if len(docName) >= MAX_STRING_LENGTH:
+        # log file name
+        writeToTooLong(str(fileID) + ". " +str(docName))
+        ###### change name of file ######
+        factor = len(docName) - MAX_STRING_LENGTH + 15
+        # build search regex
+        regex = r".{" + re.escape(str(factor)) + r"}\."
+        # replace the {x}. with just . alone
+        docName = re.sub(regex, '.', docName)
+        print(docName)
+    
+    appendNum = 0
+    while (os.path.isfile(os.path.join(dest, str(fileID) + "." + docName))):
+        appendNum += 1
+        docName = re.sub(r'([0-9]*)\.', str(appendNum) + ".", docName)
+        print(docName)
+
     finalDest = os.path.join(dest, str(fileID) + "." + docName)
     copy(src, finalDest)
 
@@ -56,7 +100,6 @@ def runCopyFile(src, folderID, ID):
     dest = os.path.join(ZIP_SAVE_PATH, str(folderID))
     DestSize = countFileSize(dest)
     DocumentsSize = countFileSize(src)
-
     # if destSize and document is larger than Max_file_Size, make new folder
     if DestSize + DocumentsSize >= MAX_FOLDER_SIZE:
         folderID += 1
@@ -86,10 +129,14 @@ if __name__ == '__main__':
         numFolderSave = 1
     
     # print(index)
-    documentIDs = os.listdir(DOCUMENT_ROOT_PATH)
-    documentIDs = list(map(int, documentIDs))
-    documentIDs.sort()
-    print(documentIDs)
+    ######################################## change below for non testing #################################
+    # documentIDs = parseJsonToIntArr()
+
+    # documentIDs = [1]
+    # print(documentIDs)
+
+    documentIDs = read_client_ids()    
+    ########################################################################################################
     for documentID in documentIDs:
         if getIndexNum() > int(documentID): continue
         DocumentFolderSrc = os.path.join(DOCUMENT_ROOT_PATH, str(documentID))
